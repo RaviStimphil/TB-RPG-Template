81f7baf3ff488c9eac7cb50670714ff79c6ea7aa
RaviStimphil <97135286+RaviStimphil@users.noreply.github.com>
2025-09-19 19:11:26 -0500

    UI Beginning Addition

---
+++ Assets/Scripts/BattleUIManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleUIManager : MonoBehaviour
+{
+    public GameObject UnitTargetArea;
+    public GameObject ActionChoiceArea;
+    public GameObject SkillChoiceArea;
+    public GameObject AllyResourceArea;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
